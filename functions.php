c4c90567e0de3cea8e2f7042097eddcfd38fb97c
rsouders <rheansouders@Rheans-MacBook-Pro.local>
2015-05-08 13:33:03 -0700

    Weeks 4 and 5 v 2
    Repeating sync to place in root

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+Theme Name: Rhean Souders' WordPress theme for Web 170
+Author: Rhean Souders
+Author URI: http://rheansouders.com
+Description: Theme for WordPress class, spring 2015
+Version: 1.0
+*/
+
+// Register Menus
+register_nav_menus(array( 'main-menu' => __( 'Main' ),
+));
+//
+
+?>
