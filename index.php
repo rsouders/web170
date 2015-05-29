130b92060f4393409b07d1ca03f634ab601dcd80
rsouders <rheansouders@Rheans-MacBook-Pro.local>
2015-05-29 12:55:45 -0700

    Week 8
    Custom posts w/ featured images

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <!-- Start Text -->
 	<div id="content">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //start loop 1 ?>
+    <article class="post-excerpt"> <!-- Contains each item; prevents Great Collapse -->
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); //include the title, and hyperlink it ?></a></h2> 
-    <?php the_content('') ?>
+    <small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>.</small>
+    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('thumbnail') //add featured image thumbnail, and permalink it ?></a>
+    <?php the_excerpt('') ?>
+    <p class="read-more"><a href="<?php the_permalink() ?>">Read more...</a></p>
+    </article>
     <?php endwhile; endif; //end loop 1 ?>   
     <small>index.php</small> 
     </div> 
--- single.php
+++ single.php
@@ -3,9 +3,12 @@
 <!-- Start Text -->
 	<div id="content">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); //start loop 1 ?>
+    <article id="post-<?php the_ID(); ?>" class="post">
     <h2><?php the_title(); //include the title, but don't hyperlink it ?></h2> 
-    <p><i>Byline will go here eventually</i></p>
+    <p><small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author(); ?> in <?php the_category(', '); ?>.</small></p>
+    <?php the_post_thumbnail('large') ?>
     <?php the_content('') ?>
+    </article>
     <?php endwhile; endif; //end loop 1 ?>   
     <small>single.php</small> 
     </div> 
